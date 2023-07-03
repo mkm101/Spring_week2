ef64271b71c3a85f9bceed315d9a32cf6eb2308c
JiHun <dksk0359@gmail.com>
2023-07-03 10:47:22 +0900

    Bean을 수동으로 등록하는 방법

--- src/main/java/com/sparta/springauth/SpringAuthApplication.java
+++ src/main/java/com/sparta/springauth/SpringAuthApplication.java
@@ -1,13 +1,12 @@
-package com.example.springauth;
+package com.sparta.springauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 제외
 public class SpringAuthApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringAuthApplication.class, args);
     }
-
 }
